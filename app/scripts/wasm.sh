b5a82eb6d964f1cb7987cf8ad1550d75c04f4f30
Jonathan Beverly <jonathan@jrbeverly.me>
2022-05-07 20:47:26 +0000

    Minor touchup after confirm

--- app/scripts/wasm.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" wasm/wasm_exec.js
-
-GO111MODULE=off GOOS=js GOARCH=wasm GOCACHE='/tmp' go build -o wasm/main.wasm ../cmd/wasm/main.go
